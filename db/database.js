85afa1a7f239cec8922551a2edb2f8888ca2c92f
Turingcop <67982783+Turingcop@users.noreply.github.com>
2022-09-25 15:44:59 +0200

    debug

--- db/database.js
+++ db/database.js
@@ -3,13 +3,11 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 const uri = `mongodb+srv://
-${process.env.ATLAS_USERNAME ?? "hej"}:${process.env.ATLAS_PASSWORD ?? "hej"}
+${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}
 @cluster0.czazvrf.mongodb.net/?retryWrites=true&w=majority`;
 
 const database = {
     getDb: async function getDb() {
-        console.log("ATLASSSSS")
-        console.log(process.env.ATLAS_USERNAME)
         const db = process.env.NODE_ENV === "test" ? "test" : "editor";
         const client = new MongoClient(uri, {
             useNewUrlParser: true,
